c53675c5829bdaaba0c390ae9c1f7c7f2640c965
Derek Fountain <derek@scratters.com>
2018-05-05 18:39:03 +0100

    01 Getting Started: black border code and TAP

---
+++ 01_GettingStarted/black_border.c
@@ -0,0 +1,13 @@
+/*
+ * Build with the newer zsdcc compiler and libraries with:
+ *
+ * zcc +zx -vn -clib=sdcc_iy -startup=31 black_border.c -o black_border -create-app
+ */
+
+#include <arch/zx.h>
+
+int main()
+{
+  zx_border(INK_BLACK);
+  return 0;
+}
