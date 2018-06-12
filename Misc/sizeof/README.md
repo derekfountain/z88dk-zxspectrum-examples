1142406b99809f5db61c69b35979d23f5749146c
Derek Fountain <derek@scratters.com>
2018-06-12 17:23:10 +0100

    Forgot short

--- Misc/sizeof/README.md
+++ Misc/sizeof/README.md
@@ -8,6 +8,7 @@ For easy reference, the values are:
 ```
   sizeof(char)         = 1
   sizeof(int)          = 2
+  sizeof(short)        = 2
   sizeof(long)         = 4
   sizeof(long long)    = 8
   sizeof(float)        = 4
--- Misc/sizeof/sizeof.c
+++ Misc/sizeof/sizeof.c
@@ -280,6 +280,7 @@ int main()
 
   printf("sizeof(char)         = %d\n", sizeof(char));
   printf("sizeof(int)          = %d\n", sizeof(int));
+  printf("sizeof(short)        = %d\n", sizeof(short));
   printf("sizeof(long)         = %d\n", sizeof(long));
   printf("sizeof(long long)    = %d\n", sizeof(long long));
   printf("sizeof(float)        = %d\n", sizeof(float));
